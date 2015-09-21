3a23a7c9191bc71c13c051d6270490e6141ed9cd
Chris <fosterchris22@gmail.com>
2015-09-21 15:03:40 -0400

    Slider animates.

--- scripts.js
+++ scripts.js
@@ -2,16 +2,40 @@ $(document).ready(function(){
 
 	var playing = "";
 	$(".music-btn").on("click", function(){
-		var current = "#" + event.target.id + " audio";
+		var target = event.target;
+		var musicSource = "";
+		var targetId = $(target).parentsUntil(".music-board").last()[0].id;
+		var slider = "#" + targetId + " .slider";
+		if ($(target).parent().attr("class") !== "music-board") {
+			musicSource = targetId;
+		}
+		else {
+			musicSource = target.id;
+		}
+		var current = "#" + musicSource + " audio";
 		if (playing === current){
+			slideStop(slider);
 			$(current)[0].pause();
 			$(current)[0].currentTime = 0;
 			playing = "";
 		}
 		else {
+			slidePlay(slider);
 			$(current)[0].play();
 			playing = current;
 		}
 	});
 	
 });
+
+var slidePlay = function(slider) {
+	$(slider).animate({
+		marginLeft: "+=72"
+	}, 500);
+}
+
+var slideStop = function(slider) {
+	$(slider).animate({
+		marginLeft: "-=72"
+	}, 500);
+}
